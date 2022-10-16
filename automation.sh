c7816139064b68df532d5aff0463189162109e41
root <root@ip-172-31-85-75.ec2.internal>
2022-10-16 15:18:54 +0000

    updated

--- automation.sh
+++ automation.sh
@@ -7,20 +7,20 @@ apt update -y
 
 #check for apache2 installation
 
-if [[ apache2 !=$(dpkg --get-selections apache2 | aws '{print $1}') ]]; then
+if [[ apache2 != $(dpkg --get-selections apache2 | awk '{print $1}') ]]; then
 	apt install apache2 -y
 fi
 
 #Check for apache2 service is running
 
-running=$(systemctl status apache2 | grep active | aws '{print $3}' | tr -d '()')
+running=$(systemctl status apache2 | grep active | awk '{print $3}' | tr -d '()')
 if [[ running != ${running} ]]; then
 	systemctl start apache2
 fi
 
 #Check for apache2 service is enabled
 enabled=$(systemctl is-enabled apche2 | grep "enabled")
-if [[ enabled != $(enabled) ]]: then
+if [[ enabled != ${enabled} ]]; then
 	systemctl enable apache2
 fi
 
